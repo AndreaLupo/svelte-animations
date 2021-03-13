b1b4ae0d1f0ca9b8e4659188c54ac534e9bf2606
Wolf Developer <lupo.andrea.93@gmail.com>
2021-03-13 18:39:16 +0100

    Different transitions in and out, events on transitions

--- src/App.svelte
+++ src/App.svelte
@@ -7,6 +7,7 @@
 
     let boxInput = '';
     let showCards = false;
+    let showParagraph = false;
 
     const progress = tweened(0, {
         delay: 0,
@@ -40,11 +41,24 @@
 <input type="text" bind:this={boxInput}>
 <button on:click={addBox} >Add</button>
 
-{#each boxes as box (box)}
-    <div transition:fly={{delay: 100, duration: 500, x: 100, y: 200}}
-    on:click={discard.bind(this, box)}>{box}</div>
-{/each}
+{#if showParagraph}
+    {#each boxes as box (box)}
+        <div transition:fly|local={{delay: 100, duration: 500, x: 100, y: 200}}
+            on:click={discard.bind(this, box)}
+            on:introstart={() => {console.log('Transition intro start')}}
+            on:introend={() => {console.log('Transition intro end')}}
+            on:outrostart={() => {console.log('Transition outro start')}}
+            on:outroend={() => {console.log('Transition outro end')}}>
+            {box}
+        </div>
+    {/each}
+{/if}
+<hr>
 
+<button on:click={() => showParagraph = !showParagraph}>Toggle</button>
+{#if showParagraph}
+    <p in:fade out:fly={{x:300}}>Can you see me?</p>
+{/if}
 <style>
     div {
         width: 10rem;
